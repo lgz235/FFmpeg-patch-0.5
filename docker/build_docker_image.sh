0022a96440fc53391273790dfee21dd1cf469b16
liguozheng <liguozheng@bjtu.edu.cn>
2021-05-13 00:04:09 +0000

    change yolo class from 80 to 9

--- docker/build_docker_image.sh
+++ docker/build_docker_image.sh
@@ -22,9 +22,9 @@ BASEDIR=$(dirname "$0")
 
 # image:build
 sudo docker build --target build -f ${BASEDIR}/${dockerfile} --network=host \
-    -t "ffmpeg-video-analytics-$build_type:build" "$BASEDIR" $(env | grep -E '_(proxy|REPO|VER)=' | sed 's/^/--build-arg /')
+    -t "ffmpeg-video-analytics-$build_type:v2.3" "$BASEDIR" $(env | grep -E '_(proxy|REPO|VER)=' | sed 's/^/--build-arg /')
 
-sudo docker build -f ${BASEDIR}/${dockerfile} --network=host -t ffmpeg-video-analytics-$build_type:$tag \
+sudo docker build -f ${BASEDIR}/${dockerfile} --network=host -t ffmpeg-video-analytics-$build_type:v2.3 \
     --build-arg http_proxy=${http_proxy} \
     --build-arg https_proxy=${https_proxy} \
     ${BASEDIR}
